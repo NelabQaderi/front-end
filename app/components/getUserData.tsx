0c7b6b6f2d4c5124a3a7a5b4e0bb92f100530272
StarkBak <sstechnologyindtec@gmail.com>
2024-09-09 20:18:23 +0430

    problem solved

--- app/components/getUserData.tsx
+++ app/components/getUserData.tsx
@@ -3,53 +3,48 @@
 import { useEffect, useState } from "react";
 
 type UserResponse = {
-  id: string;
-  username: string;
-  email: string;
-  password: string;
+  message: string;
+  user: {
+    id: string;
+    username: string;
+    email: string;
+    password: string;
+  };
 };
 
 export default function GetUserData() {
-  const [usersData, setUsersData] = useState<UserResponse>({
-    id: "",
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const [reload, setReload] = useState(true);
+  const [usersData, setUsersData] = useState<UserResponse | null>(null);
 
   useEffect(() => {
-    const getUserDataFromAPI = async () => {
-      const userData = await fetch(
-        "http://localhost:3002/api/search_user/john.doe@example.com",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+    const d = async () => {
+      const getUserDataFromAPI = async () => {
+        const userData = await fetch(
+          "http://localhost:3002/api/search_user/john.doe@example.com",
+          {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json",
+            },
+          }
+        );
 
-      const res = await userData.json();
+        const res = await userData.json();
 
-      setUsersData({
-        username: res?.username,
-        email: res?.email,
-        id: res?.id,
-        password: res.password,
-      });
+        return res;
+      };
+      const c = await getUserDataFromAPI();
+      setUsersData(c);
     };
-    getUserDataFromAPI();
-    setReload(false);
-  }, [reload]);
+
+    d();
+  }, []);
 
   return (
     <div>
       hi
-      <h1>{usersData.username}</h1>
-      <h1>{usersData.email}</h1>
-      <h1>{usersData.password}</h1>
+      <h1>hi {usersData?.user.username}</h1>
+      <h1>hi {usersData?.user.email}</h1>
+      <h1>hi {usersData?.user.password}</h1>
     </div>
   );
 }
